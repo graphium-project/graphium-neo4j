f01d32e2c194dbea72f08b3ef98c51b1c1c672f3
micwimmer <mwimmer@MOWI73.salzburgresearch.at>
2019-11-08 11:25:08 +0100

    safety check: first graph version activation resulted in NullPointerException

--- neo4j-core/src/main/java/at/srfg/graphium/neo4j/service/impl/STRTreeCacheManager.java
+++ neo4j-core/src/main/java/at/srfg/graphium/neo4j/service/impl/STRTreeCacheManager.java
@@ -191,9 +191,10 @@ implements IGraphVersionStateModifiedObserver {
 				IWayGraphVersionMetadata metadata = (IWayGraphVersionMetadata) metadataObj;
 				if (((IWayGraphVersionMetadata) metadata).getState().equals(State.ACTIVE)) {
 					// if a new graph version has been activated STRTree has to be built and cached
-					if (activeIndexCache != null && !activeIndexCache.containsKey(metadata.getGraphName()) ||
+					if (activeIndexCache == null ||
+						(activeIndexCache != null && (!activeIndexCache.containsKey(metadata.getGraphName()) ||
 						(activeIndexCache.containsKey(metadata.getGraphName()) && 
-						 activeIndexCache.get(metadata.getGraphName()).getMetadata().getValidFrom().before(metadata.getValidFrom()))) {
+						 activeIndexCache.get(metadata.getGraphName()).getMetadata().getValidFrom().before(metadata.getValidFrom()))))) {
 						log.info("Got update to rebuild STR-Tree");
 						String graphVersionName = GraphVersionHelper.createGraphVersionName(metadata.getGraphName(), metadata.getVersion());
 						activeIndexCache.put(metadata.getGraphName(), new STRTreeCacheEntry(metadata, buildTree(graphVersionName)));
